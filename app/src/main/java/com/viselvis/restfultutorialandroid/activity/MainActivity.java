77c59b1e253e33b3649126eef3b5059d74e9beb4
theviselvis <visntr@gmail.com>
2021-01-16 16:16:20 +0800

    Commit: Added a new functionality (search)
    - New API endpoint for searching query
    - New method in MainActivity.java for search functionality
    - Modifications in activty_main.xml for the search functionality
    - New strings for displays related to search functionality

--- app/src/main/java/com/viselvis/restfultutorialandroid/activity/MainActivity.java
+++ app/src/main/java/com/viselvis/restfultutorialandroid/activity/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.viselvis.restfultutorialandroid.R;
 import com.viselvis.restfultutorialandroid.adapter.MoviesAdapter;
@@ -28,16 +33,37 @@ public class MainActivity extends AppCompatActivity {
     private static Retrofit retrofit = null;
     private RecyclerView recyclerView = null;
 
+    private EditText et_search;
+    private Button btn_search;
+    private String query;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        et_search = findViewById(R.id.et_search);
+        btn_search = findViewById(R.id.btn_search);
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the content of edittext
+                query = et_search.getText().toString();
+                if (!TextUtils.isEmpty(query)) {
+                    // search using the query
+                    search();
+                } else {
+                    Toast.makeText(MainActivity.this, "Type something first!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        connectAndGetApiData();
+        // connectAndGetApiData();
     }
 
     // creates an instance of retrofit
@@ -68,4 +94,33 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    // method for generating the results from our searching
+    private void search(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(getResources().getString(R.string.base_url))
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+
+        Call<MovieResponse> call = movieApiService.getSearchResults(getResources().getString(R.string.api_key), query);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, MainActivity.this));
+                Log.d(TAG, "onResponse: Number of movies received: " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.toString());
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/viselvis/restfultutorialandroid/rest/MovieApiService.java
+++ app/src/main/java/com/viselvis/restfultutorialandroid/rest/MovieApiService.java
@@ -9,4 +9,7 @@ import retrofit2.http.Query;
 public interface MovieApiService {
     @GET("movie/top_rated")
     Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("search/movie")
+    Call<MovieResponse> getSearchResults(@Query("api_key") String apiKey, @Query("query") String query);
 }
